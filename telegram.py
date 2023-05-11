ea3f2a88bad69984b53494644701f82476d0b3dc
prabesh <prabeshshrestha90@gmail.com>
2023-05-11 15:50:42 +0545

    Files Added

---
+++ telegram.py
@@ -0,0 +1,36 @@
+import os
+import telebot
+import asyncio
+import edge_tts
+import api
+
+# Initialize the Telegram bot with your bot token
+bot = telebot.TeleBot(api.YOUR_API_KEY)
+
+# Define the file paths and voice
+#FILE_PATH = "file.txt"
+VOICE = "en-US-JennyNeural"
+OUTPUT_FILE = "output.mp3"
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Welcome to the Text-to-Speech bot! Send me a message and I'll convert it to audio.")
+
+@bot.message_handler(func=lambda message: True)
+def convert_text_to_audio(message):
+    # Get the text from the user's message
+    text = message.text.strip()
+
+    # Convert the text to audio
+    communicate = edge_tts.Communicate(text, VOICE)
+    asyncio.run(communicate.save(OUTPUT_FILE))
+
+    # Send the audio file back to the user
+    with open(OUTPUT_FILE, 'rb') as audio_file:
+        bot.send_audio(message.chat.id, audio_file)
+
+    # Delete the audio file from disk
+    os.remove(OUTPUT_FILE)
+
+if __name__ == "__main__":
+    bot.polling()
